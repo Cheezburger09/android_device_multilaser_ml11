2b0f4b81cfa2e375fbd0c8e5f62dd180af75331c
Cheezburger09 <iesusprogamer@gmail.com>
2026-07-11 15:56:13 -0300

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
+set -e
+
 DEVICE=ml11
 VENDOR=multilaser
 
-source ../../../vendor/lineage/build/tools/extract_utils.sh
-
-setup_vendor $DEVICE $VENDOR
-
-extract proprietary-files.txt "$1"
-
-setup_makefiles
+echo "Blobs já extraídos."
