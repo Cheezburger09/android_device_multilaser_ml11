16beacf005b790750e1cc6f8c6a8b788c138468a
Cheezburger09 <iesusprogamer@gmail.com>
2026-07-11 15:35:35 -0300

    Update omni_ml11.mk

--- omni_ml11.mk
+++ omni_ml11.mk
@@ -7,3 +7,11 @@ PRODUCT_NAME := omni_ml11
 PRODUCT_BRAND := Multilaser
 PRODUCT_MODEL := ML11-M7sQuadCore
 PRODUCT_MANUFACTURER := Multilaser
+
+PRODUCT_RELEASE_NAME := ML11
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.product.device=ml11 \
+    ro.product.model=ML11-M7sQuadCore \
+    ro.product.brand=Multilaser \
+    ro.product.manufacturer=Multilaser
