2b23de5512a35bc0dc37798d122b2bc92212aa7d
Cheezburger09 <iesusprogamer@gmail.com>
2026-07-11 15:51:35 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,21 +1,22 @@
 LOCAL_PATH := device/multilaser/ml11
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery.fstab:recovery/root/etc/recovery.fstab
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init.ml11.rc:root/init.ml11.rc
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=160 \
-    ro.product.model=ML11-M7sQuadCore \
-    ro.product.brand=Multilaser \
-    ro.product.device=ML11-M7sQuadCore \
-    ro.product.name=ML11-M7sQuadCore \
-    ro.product.manufacturer=Multilaser
+    $(LOCAL_PATH)/rootdir/etc/recovery.fstab:recovery/root/etc/recovery.fstab \
+    $(LOCAL_PATH)/rootdir/init.recovery.sun8i.rc:root/init.recovery.sun8i.rc \
+    $(LOCAL_PATH)/rootdir/init.sun8i.rc:root/init.sun8i.rc \
+    $(LOCAL_PATH)/rootdir/init.rc:root/init.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.rc:root/ueventd.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.sun8i.rc:root/ueventd.sun8i.rc \
+    $(LOCAL_PATH)/prebuilt/kernel:kernel
 
 PRODUCT_PACKAGES += \
-    lights.polaris \
-    audio.primary.polaris \
-    camera.polaris \
-    sensors.polaris
+    charger \
+    charger_res_images
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.secure=0 \
+    ro.adb.secure=0 \
+    ro.debuggable=1 \
+    persist.sys.usb.config=mtp,adb
+
+PRODUCT_CHARACTERISTICS := tablet
